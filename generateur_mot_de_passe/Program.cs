17bd0dbad834f5d4cd2843a2f5e58816a58f5bc5
alse <halison.andria@gmail.com>
2024-12-24 11:06:49 +0100

    Add project files.

---
+++ generateur_mot_de_passe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 - Demander la longeur du mot de passe (DemanderNombre) int longeur_mot_de_passe = ...
+            // 2 - alphabet "abcd1234" 4 "1d4e"
+            // 3 - 3 - comment choisir un caractère aléatoire 
+            // 4 - generer le mot de passe 
+            // 5 - Améliorations : choix alphabet
+        }
+    }
+}
