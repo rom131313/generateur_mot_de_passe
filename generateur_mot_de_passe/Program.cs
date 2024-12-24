d38ef66852b499c58d896bac666675484da58ae5
alse <halison.andria@gmail.com>
2024-12-24 16:49:38 +0100

    3 - Enrichissement de l'alphabet

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -5,8 +5,7 @@ namespace MyApp
 {
     internal class Program
     {
-       
-
+ 
         static void Main(string[] args)
         {
             // 1 - Demander la longeur du mot de passe (DemanderNombre) int longeur_mot_de_passe = ...
@@ -15,6 +14,26 @@ namespace MyApp
             int longeurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
             // 2 - alphabet "abcd1234" 4 "1d4e"
             // 3 - 3 - comment choisir un caractère aléatoire 
+
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+            string majuscules = minuscules.ToUpper();
+            string chiffres = "0123456789";
+            string caracteresSpeciaux = "#&+-";
+            string alphabet = minuscules + majuscules + chiffres + caracteresSpeciaux;
+            string motDePasse = "";
+            int longeurAlphabet = alphabet.Length;
+            Random rand = new Random();
+
+            // boucler sur longueurMotDePasse
+            for (int i = 0; i < longeurMotDePasse; i++)
+            {
+                int index = rand.Next(0, longeurAlphabet);
+                motDePasse += alphabet[index];
+                Console.WriteLine(alphabet[index]);
+            }
+           
+            Console.WriteLine(motDePasse);
+
             // 4 - generer le mot de passe 
             // 5 - Améliorations : choix alphabet
         }
